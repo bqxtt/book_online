915e894f4c4c25abfcf75b809dffd85b664a4263
bqxtt <1486126243@qq.com>
2021-01-09 15:32:37 +0800

    cors

--- api/router/middleware/middleware.go
+++ api/router/middleware/middleware.go
@@ -9,6 +9,11 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
 		c.Next()
 	}
 }
