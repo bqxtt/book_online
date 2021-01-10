b45fbc8b9810bc963bca182739a52be4c0ce58bf
bqxtt <1486126243@qq.com>
2021-01-10 14:19:45 +0800

    rpc set

--- api/bootstrap/init.go
+++ api/bootstrap/init.go
@@ -1,8 +1,12 @@
 package bootstrap
 
 import (
+	"fmt"
 	"github.com/bqxtt/book_online/api/router"
+	"github.com/bqxtt/book_online/rpc/clients/rpc_book"
 	"github.com/bqxtt/book_online/rpc/clients/rpc_user"
+	"os"
+	"strings"
 )
 
 func Init() {
@@ -11,17 +15,19 @@ func Init() {
 }
 
 func initRpc() {
-	//rpcs := os.Getenv("rpc")
-	//if rpcs == "" {
-	//	return
-	//}
-	//rpc := strings.Split(rpcs, ".")
-	//funcs := make(map[string]func(), 0)
-	//funcs["user"] = rpc_user.Init
-	//funcs["book"] = rpc_book.Init
-	//for _, r := range rpc {
-	//	funcs[r]()
-	//}
-	rpc_user.Init()
+	rpcs := os.Getenv("rpc")
+	if rpcs == "" {
+		return
+	}
+	rpc := strings.Split(rpcs, ".")
+	funcs := make(map[string]func(), 0)
+	funcs["user"] = rpc_user.Init
+	funcs["book"] = rpc_book.Init
+
+	for _, r := range rpc {
+		fmt.Println(r)
+		funcs[r]()
+	}
+	//rpc_user.Init()
 	//rpc_book.Init()
 }
